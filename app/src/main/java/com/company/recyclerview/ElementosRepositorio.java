5878ad33f4709f308b5acb4ff9b181853217ef77
gerardfp <gerardfp@gmail.com>
2020-10-29 12:20:36 +0100

    Initial commit

--- app/src/main/java/com/company/recyclerview/ElementosRepositorio.java
+++ app/src/main/java/com/company/recyclerview/ElementosRepositorio.java
@@ -10,10 +10,12 @@ public class ElementosRepositorio {
     MutableLiveData<List<Elemento>> elementos = new MutableLiveData<>(new ArrayList<>());
 
     ElementosRepositorio(){
-        anyadir(new Elemento("hola", "que tal estas"));
-        anyadir(new Elemento("adios", "que tal esdfadsatas"));
-        anyadir(new Elemento("buenas", "que taldsfaf estas"));
-        anyadir(new Elemento("goodbye", "que tal est dsfasd  dsfas"));
+        anyadir(new Elemento("Elemento químico", "Es un átomo con moléculas, aquella sustancia que no puede ser descompuesta mediante una reacción química, en otras más simples. Pueden existir dos átomos de un mismo elemento con características distintas y, en el caso de que estos posean número másico distinto, pertenecen al mismo elemento pero en lo que se conoce como uno de sus isótopos."));
+        anyadir(new Elemento("Elemento de un conjunto", "En teoría de conjuntos, un elemento o miembro de un conjunto (o familia de conjuntos) es un objeto que forma parte de ese conjunto (o familia)."));
+        anyadir(new Elemento("Elemento sintético", "En química, un elemento sintético es un elemento químico que no aparece de forma natural en la Tierra, y solo puede ser creado artificialmente."));
+        anyadir(new Elemento("Elemento algebraico", "En matemáticas, más concretamente en álgebra abstracta y teoría de cuerpos, se dice que un elemento es algebraico sobre un cuerpo si es raíz de algún polinomio con coeficientes en dicho cuerpo. Los elementos algebraicos sobre el cuerpo de los números racionales reciben el nombre de números algebraicos."));
+        anyadir(new Elemento("Elementos de la naturaleza","Los cuatro o cinco elementos de la naturaleza —normalmente agua, tierra, fuego y aire, a los que se añade la quintaesencia o éter— eran, para muchas doctrinas antiguas, los constituyentes básicos de la materia y explicaban el comportamiento de la naturaleza. El modelo estuvo vigente hasta que la ciencia moderna empezó a desentrañar los elementos y reacciones químicas."));
+        anyadir(new Elemento("Elemento constructivo","Un elemento constructivo es cada uno de los componentes materiales que integran una obra de construcción. Se suelen clasificar en estructurales y compartimentadores."));
     }
 
     void anyadir(Elemento elemento){
